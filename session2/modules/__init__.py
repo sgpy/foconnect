3bfd0741b3b09c17308450b6a50b78d4946f9bd4
nandan-ashutosh <57680096+nandan-ashutosh@users.noreply.github.com>
2019-11-20 15:46:01 +0800

    Create __init__.py

---
+++ session2/modules/__init__.py
@@ -0,0 +1,8 @@
+"""
+"modules" pacakge from session2.
+Carries modules describing the how to create and import python modules(files)
+
+
+Python PEP 8 conventions restrict us from  using "Modules" as a name for our module.
+causing confusing use of "modules" as a proper noun and as a common noun, in the description above !!!. 
+"""
